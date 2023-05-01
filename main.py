69c8bb86b4a2b51a4bb53affbad8a0428445325e
Cp Dong <bubbu0129@gmail.com>
2023-05-01 11:27:49 +0800

    Alter layout

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 # coding: utf-8
 
 # Initialize
-font_file   = "font/MiloTE.ttf"
-fontI_file  = "font/MiloTE-inclined.ttf"
+icon        = "assets/economist.png"
+font_file   = "assets/MiloTE.ttf"
+fontI_file  = "assets/MiloTE-inclined.ttf"
 font_name   = "MiloTE"
 fontI_name  = "MiloTE-I"
 
@@ -11,20 +12,20 @@ user_agent  = "Mozilla/5.0"
 
 import sys, getopt, re, urllib.request, json
 from datetime import datetime
+from pathlib import Path
 import fpdf
 
 # getopt
 def usage(name):
     print("""Usage: %s [OPTIONS]
-The program converts articles on the Economist to PDF.
+The program converts articles on the Economist to PDF. 
 It reads URLs from <stdin> in format "<URL>\\n<URL>\\n<URL>\\n..."
 Options: 
     -d, --dir=          Local directory to store output file
     -p, --http-proxy=   HTTP proxy to fetch article
     -t, --text-only     Output text-only PDF
-        --url-prefix=   URL prefix to access output file
     -v, --verbose       Print user-friendly message
-    -q, --quiet         Redirect <stdout>
+    -q, --quiet         Redirect <stdout> to NULL
     -h, --help          Show help message
 """ % (name), end="")
 
@@ -33,7 +34,6 @@ argv = sys.argv
 proxy_handler = None
 base_dir = ""
 text_only = False
-url_prefix = ""
 verbosity = 0
 try:
     opts, args = getopt.getopt(argv[1:], \
@@ -52,8 +52,6 @@ for opt, arg in opts:
         proxy_handler = urllib.request.ProxyHandler({"http": arg, "https": arg})
     elif opt in ("-t", "--text-only"):
         text_only = True
-    elif opt in ("--url-prefix"):
-        url_prefix = arg if arg.endswith('/') else (arg + '/')
     elif opt in ("-v", "--verbose"):
         verbosity = 1
     elif opt in ("-q", "--quiet"):
@@ -69,11 +67,11 @@ urllib.request.install_opener(opener)
 
 # fetch html & generate PDF
 urlArr = stdin.splitlines()
+regex = r"^https:\/\/www\.economist\.com\/(.*)\/(\d{4})\/(\d{2})\/(\d{2})\/(.*?)(?=\?|\Z).*"
+pattern = re.compile(regex)
 
 def main(url):
 
-    regex = r"^https:\/\/www\.economist\.com\/(.*)\/(\d{4})\/(\d{2})\/(\d{2})\/(.*?)(?=\?|\Z).*"
-    pattern = re.compile(regex)
     if not pattern.fullmatch(url):
         return ""
     cat_dir = pattern.sub("\\1/", url)
@@ -82,45 +80,60 @@ def main(url):
 
     req = urllib.request.Request(url)
     req.add_header('User-agent', user_agent)
-    response = urllib.request.urlopen(req)
-    response.readline()
-    b = response.readline()
-    data = json.loads(b.decode())
+    content = urllib.request.urlopen(req).readlines()
+    data = json.loads(content[1].decode())
     body = data['articleBody'].split('\n')
+    date = datetime.strptime(data['datePublished'], "%Y-%m-%dT%H:%M:%SZ")
+    subheadline = re.search(r"\"shareSnippet\":\"(.*) –", content[-2].decode()).group(1)
     urllib.request.urlretrieve(data['thumbnailUrl'], "/tmp/thumbnail.png")
 
     headline_ascii = data['headline'].encode("ascii", errors="ignore").decode() # Some Unicode characters cannot be stored in PDF's metadata
 
-    html = "<h1>%s</h1>" % (data["headline"],)
+    html = str()
+    html += "<h1>%s</h1>" % (data["headline"],)
     html += "<font face=\"%s\" size=16><p line-height=2>%s</p></font>" % (fontI_name, data["description"])
-    if not text_only:
-        html += "<p align=R><font color=#0000ff><u><a href=\"%s\">Webpage</a></u></font></p>" % (data["url"],)
+    html += "<p align=\"right\">Published on %s</p>" % (date.strftime("%b %d %Y, %I:%M %p"))
+    if text_only:
+#        html += "<p>---------------------------------------------------------------------------------------------------------------------------</p>"
+        pass
+    else:
         html += "<img src=\"/tmp/thumbnail.png\" width=538>"    # pixel width of an A4 page
     for line in body:
-        html += "<p line-height=1.5>%s</p>" % (line,)
-        if not line:
-            continue
-        if (line[-1] == '■'):
+        if (line and line[-1] == '■'):
+            html += "<p line-height=1.5>%s</p>" % (line[:-1],)
             break
+        html += "<p line-height=1.5>%s</p>" % (line,)
+
+    class PDF(fpdf.FPDF):
+        def header(self):
+            self.image(icon, 10, 10, 20, link="https://www.economist.com/")
+            self.cell(0, 10, "%s | %s" % (data['articleSection'], subheadline), align="R", link=data["url"])
+            # Line Break:
+            self.ln(16)
+
+        def footer(self):
+            self.set_y(-15)
+            self.cell(0, 10, f"{self.page_no()}/{{nb}}", align="R")
 
     # generate PDF by html
-    pdf = fpdf.FPDF()
+    pdf = PDF()
     pdf.add_font(font_name,'',font_file)
     pdf.add_font(fontI_name,'',fontI_file)
     pdf.set_font(font_name)
     pdf.set_title(headline_ascii)
     pdf.set_author(data['author']['name'])
     pdf.set_creator(data['creator']['name'])
-    pdf.set_producer("fpdf2 " + fpdf.FPDF_VERSION)
-    pdf.set_creation_date(datetime.strptime(data['dateCreated'], "%Y-%m-%dT%H:%M:%SZ"))
+    pdf.set_producer("Python fpdf2 %s" % (fpdf.FPDF_VERSION,))
+    pdf.set_creation_date(date)
     pdf.set_lang(data['inLanguage'])
     pdf.set_keywords(data['keywords'])
     pdf.set_subject(data['articleSection'])
     pdf.add_page()
+    pdf.ln(-8)
     pdf.write_html(html)
     pdf.output(base_dir + cat_dir + filename)
 
-    return (url_prefix + cat_dir + filename)
+    return (cat_dir + filename)
 
 if __name__ == "__main__":
     if not urlArr:
@@ -134,6 +147,5 @@ if __name__ == "__main__":
         for item in urlArr:
             print(main(item), end='')
     else:
-        print("Ctrl + Click to open file.")
         for item in urlArr:
             print(main(item))
